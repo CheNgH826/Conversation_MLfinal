ccdad5ada99fb0f21092cfd297ff9fc0cc0f2217
CheNgH826 <lilioo826@gmail.com>
2017-12-15 22:11:45 +0800

    te

---
+++ data/w2v_train.py
@@ -0,0 +1,20 @@
+from gensim.models import Word2Vec
+import jieba
+
+corpus = []
+for i in range(1,6):
+    corpus.append('training_data/'+str(i)+'_train.txt')
+
+sen_list = []
+for one_corpus in corpus:
+    with open(one_corpus, 'r') as f:
+        sens = list(f)
+        for sen in sens:
+            sen = sen.replace('\n', '')
+            sen_word_list = list(jieba.cut(sen))
+            sen_list.append(list(sen_word_list))
+
+WORDVEC_DIM = 300
+w2v_model = Word2Vec(sen_list, min_count=1, size=WORDVEC_DIM, iter=20)
+w2v_model.init_sims(replace=True)
+w2v_model.save('w2v.mdl')
